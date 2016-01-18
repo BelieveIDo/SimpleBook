31143e7be695f0516fd730396d4d935eafddb3ba
wangfei <wangfeijn@163.com>
2016-01-18 15:48:48 +0800

    umeng

--- app/src/main/java/com/wangfei/simplebook/ThirdPartyLoginActivity.java
+++ app/src/main/java/com/wangfei/simplebook/ThirdPartyLoginActivity.java
@@ -1,23 +1,63 @@
 package com.wangfei.simplebook;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
+
+import com.umeng.socialize.UMAuthListener;
+import com.umeng.socialize.UMShareAPI;
+import com.umeng.socialize.bean.SHARE_MEDIA;
+
+import java.util.Map;
 
 public class ThirdPartyLoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private UMShareAPI mShareAPI;
+    private SHARE_MEDIA platform;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third_party_login);
+        initUmeng();
     }
+
+    private void initUmeng(){
+        //        首先获取UMShareAPI
+        mShareAPI = UMShareAPI.get(this);
+
+
+        platform = SHARE_MEDIA.SINA;
+        mShareAPI.doOauthVerify(this, platform, umAuthListener);
+
+    }
+    private UMAuthListener umAuthListener = new UMAuthListener() {
+        @Override
+        public void onComplete(SHARE_MEDIA platform, int action, Map<String, String> data) {
+            Toast.makeText(getApplicationContext(), "Authorize succeed", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onError(SHARE_MEDIA platform, int action, Throwable t) {
+            Toast.makeText( getApplicationContext(), "Authorize fail", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onCancel(SHARE_MEDIA platform, int action) {
+            Toast.makeText( getApplicationContext(), "Authorize cancel", Toast.LENGTH_SHORT).show();
+        }
+    };
 
 //       QQ登录
     public void QQClick(View view) {
 
+
     }
 //      新浪登录
     public void SinaClick(View view) {
+
 
     }
 //      微信登录
@@ -33,5 +73,17 @@ public class ThirdPartyLoginActivity extends AppCompatActivity implements View.O
     public void onClick(View v) {
         switch (v.getId()){
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mShareAPI.deleteOauth(ThirdPartyLoginActivity.this, platform,  umAuthListener);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mShareAPI.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/com/wangfei/simplebook/application/BaseApplication.java
+++ app/src/main/java/com/wangfei/simplebook/application/BaseApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.activeandroid.ActiveAndroid;
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.umeng.socialize.PlatformConfig;
 
 /**
  * Created By: AndroidStudio [FR]
@@ -18,6 +19,26 @@ public class BaseApplication extends Application{
         Fresco.initialize(this);
         //ActiveAndroid初始化
         ActiveAndroid.initialize(this);
+
+        initUmeng();
+    }
+
+    private void initUmeng() {
+//        PlatformConfig.setWeixin("wx967daebe835fbeac", "5bb696d9ccd75a38c8a0bfe0675559b3");
+//        //微信 appid appsecret
+        PlatformConfig.setSinaWeibo("3921700954","04b48b094faeb16683c32669824ebdad");
+        //新浪微博 appkey appsecret
+//        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+//        // QQ和Qzone appid appkey
+//        PlatformConfig.setAlipay("2015111700822536");
+//        //支付宝 appid
+//        PlatformConfig.setYixin("yxc0614e80c9304c11b0391514d09f13bf");
+//        //人人 appid appkey appsecret
+//        PlatformConfig.setTwitter("3aIN7fuF685MuZ7jtXkQxalyi", "MK6FEYG63eWcpDFgRYw4w9puJhzDl0tyuqWjZ3M7XJuuG7mMbO");
+//        //Twitter appid appkey
+//        PlatformConfig.setPinterest("1439206");
+//        //Pinterest appid
+//        PlatformConfig.setLaiwang("laiwangd497e70d4", "d497e70d4c3e4efeab1381476bac4c5e");
     }
 
     @Override
@@ -26,4 +47,6 @@ public class BaseApplication extends Application{
         //ActiveAndroid清理
         ActiveAndroid.dispose();
     }
+
+
 }
