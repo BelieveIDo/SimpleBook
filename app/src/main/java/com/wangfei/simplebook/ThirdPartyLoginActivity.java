ed92fa47e326d9dfb332304e4f6de17fdad3a491
wangfei <wangfeijn@163.com>
2016-01-18 11:49:55 +0800

    增加布局

--- app/src/main/java/com/wangfei/simplebook/ThirdPartyLoginActivity.java
+++ app/src/main/java/com/wangfei/simplebook/ThirdPartyLoginActivity.java
@@ -3,25 +3,43 @@ package com.wangfei.simplebook;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
-public class ThirdPartyLoginActivity extends AppCompatActivity {
+public class ThirdPartyLoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mBackTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third_party_login);
+        mBackTextView = ((TextView) findViewById(R.id.tv));
+        mBackTextView.setOnClickListener(this);
     }
 
 //       QQ登录
     public void QQClick(View view) {
+
     }
 //      新浪登录
     public void SinaClick(View view) {
+
     }
 //      微信登录
     public void WeChatClick(View view) {
+
     }
 //      一键分享
     public void ShareClick(View view) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tv:
+                finish();
+                break;
+        }
     }
 }
