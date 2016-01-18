864908db582215a952a0c35343acd94f1c71009b
wangfei <wangfeijn@163.com>
2016-01-18 14:46:07 +0800

    哈哈

--- app/src/main/java/com/wangfei/simplebook/ThirdPartyLoginActivity.java
+++ app/src/main/java/com/wangfei/simplebook/ThirdPartyLoginActivity.java
@@ -1,20 +1,15 @@
 package com.wangfei.simplebook;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
 
 public class ThirdPartyLoginActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private TextView mBackTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third_party_login);
-        mBackTextView = ((TextView) findViewById(R.id.tv));
-        mBackTextView.setOnClickListener(this);
     }
 
 //       QQ登录
@@ -37,9 +32,6 @@ public class ThirdPartyLoginActivity extends AppCompatActivity implements View.O
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.tv:
-                finish();
-                break;
         }
     }
 }
